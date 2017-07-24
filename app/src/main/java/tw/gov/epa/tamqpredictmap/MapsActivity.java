99a7d9d483fe84ffb4489a68aab4f233f6610e9c
c11wcz <c11wcz@mail.com>
2017-07-24 16:22:17 +0800

    bu

--- app/src/main/java/tw/gov/epa/tamqpredictmap/MapsActivity.java
+++ app/src/main/java/tw/gov/epa/tamqpredictmap/MapsActivity.java
@@ -1,18 +1,79 @@
 package tw.gov.epa.tamqpredictmap;
 
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.Gradient;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    /**
+     * Alternative radius for convolution
+     */
+    private static final int ALT_HEATMAP_RADIUS = 10;
+
+    /**
+     * Alternative opacity of heatmap overlay
+     */
+    private static final double ALT_HEATMAP_OPACITY = 0.4;
+
+    /**
+     * Alternative heatmap gradient (blue -> red)
+     * Copied from Javascript version
+     */
+    private static final int[] ALT_HEATMAP_GRADIENT_COLORS = {
+            Color.argb(0, 0, 255, 255),// transparent
+            Color.argb(255 / 3 * 2, 0, 255, 255),
+            Color.rgb(0, 191, 255),
+            Color.rgb(0, 0, 127),
+            Color.rgb(255, 0, 0)
+    };
+
+    public static final float[] ALT_HEATMAP_GRADIENT_START_POINTS = {
+            0.0f, 0.10f, 0.20f, 0.60f, 1.0f
+    };
+
+    public static final Gradient ALT_HEATMAP_GRADIENT = new Gradient(ALT_HEATMAP_GRADIENT_COLORS,
+            ALT_HEATMAP_GRADIENT_START_POINTS);
+
+    private HeatmapTileProvider mProvider;
+    private TileOverlay mOverlay;
+
+    private boolean mDefaultGradient = true;
+    private boolean mDefaultRadius = true;
+    private boolean mDefaultOpacity = true;
+
+    /**
+     * Maps name of data set to data (list of LatLngs)
+     * Also maps to the URL of the data set for attribution
+     */
+    private HashMap<String, DataSet> mLists = new HashMap<String, DataSet>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,11 +101,79 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(24, 121), 7));
 
-        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(24, 121);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-//        mMap.animateCamera(CameraUpdateFactory.zoomTo(7), 1000, null);
+        PolygonOptions rectOptions = new PolygonOptions()
+                .add(new LatLng(24.35, 121.0),
+                        new LatLng(24.45, 121.0),
+                        new LatLng(24.45, 121.2),
+                        new LatLng(24.35, 121.2),
+                        new LatLng(24.35, 121.0))
+                .fillColor(Color.argb(255 / 3 * 2, 0, 255, 255))
+                .strokeColor(Color.alpha(0));
+
+        PolygonOptions rectOptions2 = new PolygonOptions()
+                .add(new LatLng(24.32, 121.0),
+                        new LatLng(24.40, 121.0),
+                        new LatLng(24.40, 121.1),
+                        new LatLng(24.35, 121.1),
+                        new LatLng(24.32, 121.0))
+                .fillColor(Color.argb(100,255, 0, 0))
+                .strokeColor(Color.alpha(0));
+
+        // Get back the mutable Polygon
+        Polygon polygon = mMap.addPolygon(rectOptions);
+        mMap.addPolygon(rectOptions2);
+
+//        try {
+//            mLists.put("police_station", new DataSet(readItems(R.raw.police)));
+//        } catch (JSONException e) {
+//            Toast.makeText(this, "Problem reading list of markers.", Toast.LENGTH_LONG).show();
+//        }
+//
+//        // Check if need to instantiate (avoid setData etc twice)
+//        if (mProvider == null) {
+//            mProvider = new HeatmapTileProvider.Builder()
+//                    .data(mLists.get("police_station").getData())
+//                    .gradient(ALT_HEATMAP_GRADIENT)
+//                    .build();
+//            mOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
+//            // Render links
+////            attribution.setMovementMethod(LinkMovementMethod.getInstance());
+//        } else {
+////            mProvider.setData(mLists.get(dataset).getData());
+////            mOverlay.clearTileCache();
+//        }
+
 
     }
+
+    /**
+     * Helper class - stores data sets and sources.
+     */
+    private class DataSet {
+        private ArrayList<LatLng> mDataset;
+
+        public DataSet(ArrayList<LatLng> dataSet) {
+            this.mDataset = dataSet;
+        }
+
+        public ArrayList<LatLng> getData() {
+            return mDataset;
+        }
+    }
+
+    // Datasets from http://data.gov.au
+    private ArrayList<LatLng> readItems(int resource) throws JSONException {
+        ArrayList<LatLng> list = new ArrayList<LatLng>();
+        InputStream inputStream = getResources().openRawResource(resource);
+        String json = new Scanner(inputStream).useDelimiter("\\A").next();
+        JSONArray array = new JSONArray(json);
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject object = array.getJSONObject(i);
+            double lat = object.getDouble("lat");
+            double lng = object.getDouble("lng");
+            list.add(new LatLng(lat, lng));
+        }
+        return list;
+    }
+
 }
